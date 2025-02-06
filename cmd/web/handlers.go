efa3054c2d56531db5e1d430caf1bf419e6c338c
Cerecero <ale.rey.cer@outlook.com>
2025-02-06 12:42:40 -0600

    removed the checking of POST at the begining of the function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -48,11 +48,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
 	title := "0 snail"
 	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
 	expires := 7
