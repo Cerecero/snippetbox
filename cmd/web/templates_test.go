8888b8aded70e16c7141a0241d347cf4dbe4b2dd
Cerecero <ale.rey.cer@outlook.com>
2025-02-14 19:33:38 -0600

    addede templates_test.go

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestHumanDate(t *testing.T) {
+	// Anonytmous structs containint the test case
+	tests := []string {
+
+	}
+
+	tm := time.Date(2022, 3, 17, 10, 15, 0, 0, time.UTC)
+	hd := humanDate(tm)
+
+	if hd != "17 Mar 2022 at 10:15" {
+		t.Errorf("got %q: want %q", hd, "17 Mar 2022 at 10:15")
+	}
+}
